5b9e17c453e88b6ae60e8e764ef555dce754e68e
Rishikesh Patel <patelrishikesh2095@gmail.com>
2021-06-10 13:45:41 +0530

    landing page ui update

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import illustration from '../media/illustration.png';
+import photo from '../media/photo.jpg';
+import dashkit from '../media/dashkit.jpg';
 function Image() {
   return (
     <div className='landing-page'>
@@ -28,7 +30,7 @@ function Image() {
           </div>
         </div>
         <div className='child'>
-          <img src={illustration} alt='img' />
+          <img src={illustration} alt='illustration' />
         </div>
       </div>
       <div className='welcome-page'>
@@ -113,7 +115,6 @@ function Image() {
           </div>
         </div>
       </div>
-
       <div
         className='welcome-page'
         style={{
@@ -176,6 +177,110 @@ function Image() {
               fill-rule='nonzero'
             ></path>
           </svg>
+        </div>
+      </div>
+      <div className='welcome-page' style={{ backgroundImage: 'linear-gradient(180deg,#f9fbfd 0,#fff)' }}>
+        <div className='child login-container'>
+          <div className='login-card'>
+            <img src={photo} alt='test' />
+            <form>
+              <input className='form-input' required type='text' id='fname' name='name' placeholder='Name' />
+              <input className='form-input' required type='email' id='email' name='email' placeholder='Email' />
+              <input
+                className='form-input'
+                required
+                type='password'
+                id='password'
+                name='password'
+                placeholder='Password'
+              />
+              <br />
+              <br />
+              <input className='submit-button' type='submit' value='Download a sample' />
+            </form>
+          </div>
+        </div>
+        <div className='child' style={{ marginTop: '7rem' }}>
+          <h3>The most useful resource</h3>
+          <p>
+            Using Landkit to build your site means never worrying about designing another page or cross browser
+            compatibility. Our ever-growing library of components and pre-designed layouts will make your life easier.
+          </p>
+          <div className='text-list'>
+            <span>Lifetime updates</span>
+            <span>Tons of assets</span>
+            <span>Tech support</span>
+            <span>Integration ready</span>
+          </div>
+        </div>
+      </div>
+      <div className='welcome-page '>
+        <div className='child'>
+          <h2>
+            We have lots of experience Landkit. <br />
+            <span style={{ color: '#335eea' }}>building Bootstrap themes</span>.
+          </h2>
+          <p>We've built well over a dozen Bootstrap themes and sold tens of thousands of copies.</p>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div className='icon-container' style={{ width: '5rem' }}>
+              <svg width='24' height='24' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                <g fill='none' fill-rule='evenodd'>
+                  <path d='M0 0h24v24H0z'></path>
+                  <path
+                    d='M12 8H8a4 4 0 00-4 4v1a3 3 0 003 3v2a5 5 0 01-5-5v-1a6 6 0 016-6h4V4.728a.5.5 0 01.8-.4l2.856 2.133a.5.5 0 01-.001.802l-2.857 2.121A.5.5 0 0112 8.983V8z'
+                    fill='#335EEA'
+                  ></path>
+                  <path
+                    d='M12.058 16H16a4 4 0 004-4v-1a3 3 0 00-3-3V6a5 5 0 015 5v1a6 6 0 01-6 6h-3.942v.983a.5.5 0 01-.798.401l-2.857-2.12a.5.5 0 010-.803l2.856-2.134a.5.5 0 01.8.401V16z'
+                    fill='#335EEA'
+                    opacity='.3'
+                  ></path>
+                </g>
+              </svg>
+            </div>
+            <div>
+              <div className='title-text'>
+                <h3>Bootstrap users since the beginning</h3>
+              </div>
+              <div className='discription'>
+                <p>We've been developing with Bootstrap since it was publicly released in 2011.</p>
+              </div>
+            </div>
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <div className='icon-container' style={{ width: '6rem' }}>
+              <svg width='24' height='24' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                <g fill='none' fill-rule='evenodd'>
+                  <path d='M0 0h24v24H0z'></path>
+                  <path
+                    d='M17.272 8.685a1 1 0 111.456-1.37l4 4.25a1 1 0 010 1.37l-4 4.25a1 1 0 01-1.456-1.37l3.355-3.565-3.355-3.565zm-10.544 0L3.373 12.25l3.355 3.565a1 1 0 01-1.456 1.37l-4-4.25a1 1 0 010-1.37l4-4.25a1 1 0 011.456 1.37z'
+                    fill='#335EEA'
+                  ></path>
+                  <rect
+                    fill='#335EEA'
+                    opacity='.3'
+                    transform='rotate(15 12 12)'
+                    x='11'
+                    y='4'
+                    width='2'
+                    height='16'
+                    rx='1'
+                  ></rect>
+                </g>
+              </svg>
+            </div>
+            <div>
+              <div className='title-text'>
+                <h3>Deep understanding of Bootstrap</h3>
+              </div>
+              <div className='discription'>
+                <p>We've watched Bootstrap grow up over the years and understand it better than almost anyone.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className='child'>
+          <img src={dashkit} alt='dashboard' />
         </div>
       </div>
     </div>
