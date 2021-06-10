90a0bb4f6e2689984814c4345cd61c41995b50e5
Rishikesh Patel <patelrishikesh2095@gmail.com>
2021-06-10 08:43:23 +0530

    add user details page

--- src/components/GoogleList.jsx
+++ src/components/GoogleList.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function BookCard(param) {
-  const { book } = param;
+function Card(param) {
+  const { data } = param;
+
   return (
     <div className='book-container'>
       <div className='book'>
-        <div className='book-title'>{book.volumeInfo.title}</div>
-        <div className='book-subtitle'>{book.volumeInfo.subtitle}</div>
-        <div className='book-publisher'>Published By -{book.volumeInfo.publisher}</div>
+        <div className='book-title'>{data.volumeInfo.title}</div>
+        <div className='book-subtitle'>{data.volumeInfo.subtitle}</div>
+        <div className='book-publisher'>Published By -{data.volumeInfo.publisher}</div>
       </div>
     </div>
   );
@@ -16,23 +17,19 @@ function BookCard(param) {
 
 function GoogleList() {
   const [books, setBooks] = useState([]);
+  const [apiKey] = useState('AIzaSyAakDEFOJdtgURYVwfO_h3L_Bftc4savR4');
   useEffect(() => {
-    axios
-      .get(
-        'https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyAakDEFOJdtgURYVwfO_h3L_Bftc4savR4'
-      )
-      .then((response) => {
-        console.log(response);
-        setBooks(response.data.items);
-      });
-  }, []);
+    axios.get(`https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=${apiKey}`).then((response) => {
+      setBooks(response.data.items);
+    });
+  });
 
   return (
     <div className='list-container'>
       <h1>Books</h1>
       <div className='list'>
         {books.map((book) => {
-          return <BookCard book={book} />;
+          return <Card data={book} />;
         })}
       </div>
     </div>
--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,7 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function UserDetails() {
-  return <div>User Details</div>;
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [address, setAddress] = useState('');
+  const [showUserDetails, setShowUserDetails] = useState(false);
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setShowUserDetails(true);
+  };
+  return (
+    <div className='user-details-container'>
+      <h1>User Details</h1>
+      <div className='form-container'>
+        <form onSubmit={(data) => handleFormSubmit(data)} className='user-details-from'>
+          <input
+            className='form-input'
+            required
+            type='text'
+            id='fname'
+            name='name'
+            placeholder='Name'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <input
+            className='form-input'
+            required
+            type='email'
+            id='email'
+            name='email'
+            placeholder='Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <input
+            className='form-input'
+            required
+            type='number'
+            id='phone'
+            name='phone'
+            placeholder='Phone'
+            value={phone}
+            style={{ backgroundColor: '#f1f1f1' }}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+
+          <input
+            className='form-input'
+            required
+            type='textarea'
+            id='address'
+            name='address'
+            placeholder='Address'
+            value={address}
+            onChange={(e) => setAddress(e.target.value)}
+          />
+          <br />
+          <br />
+          <input className='submit-button' type='submit' value='Submit' />
+        </form>
+      </div>
+      {showUserDetails && (
+        <div className='user-details-card'>
+          <div className='book-title '>{name}</div>
+          <div className='book-subtitle'>Address: {address}</div>
+          <div className='book-subtitle'>Email: {email}</div>
+          <div className='user-phone'>{phone}</div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default UserDetails;
